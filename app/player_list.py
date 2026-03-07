ccb902aac949a4603e0744cc0e155dfc59585800
Khushboo Dayal <20128654@tafe.wa.edu.au>
2026-03-07 20:14:19 +0800

    Step 5: Added is_empty and insert_head methods

--- app/player_list.py
+++ app/player_list.py
@@ -17,3 +17,12 @@ class PlayerList:
             new_node.next = self._head
             self._head.prev = new_node
             self._head = new_node   
+    def insert_tail(self, player):
+        new_node = PlayerNode(player)
+        if self.is_empty():
+            self._head = new_node
+            self._tail = new_node
+        else:
+            new_node.prev = self._tail
+            self._tail.next = new_node
+            self._tail = new_node
